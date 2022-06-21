c4d762e49701846cc4ed532090211f36c4938dbe
Gustavo Teixeira <contact@gustavoteixeira.dev>
2022-06-21 15:35:12 -0300

    feat: added base project for the api

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# EasyGest
+# EasyGest  
+
+
+### Protótipo navegável
+
+Wireframe desenvolvido utilizando o Figma:
+
+https://www.figma.com/file/5Ee6a9I7EH1zIssac2Us3y/EasyGest?node-id=0%3A1
---
+++ api/easygest/src/main/java/dev/gustavoteixeira/easygest/EasygestApplication.java
@@ -0,0 +1,13 @@
+package dev.gustavoteixeira.easygest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasygestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasygestApplication.class, args);
+	}
+
+}
